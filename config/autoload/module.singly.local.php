c18f6096a645e320a5f58b00019435fc3c2dd773
Tom Anderson <tom.h.anderson@gmail.com>
2012-12-11 11:44:23 -0800

    zf skeleton in place

---
+++ config/autoload/module.singly.local.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Singly;
+
+return array(
+    'singly' => array(
+        'client_id' => "",
+        'client_secret' => "",
+        'redirect_uri' => "http://localhost:8080/singly/takelogin",
+        'services' => array(
+            'fitbit' => array(
+                'name' => 'Fitbit',
+                'icon' => '/assets/Singly/images/fitbit.png',
+            ),
+        ),
+    ),
+);
+
