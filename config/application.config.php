5b915dcfe7ba367122496b7fef8d4e8d61ea8543
Tom Anderson <tom.h.anderson@gmail.com>
2012-12-11 11:58:11 -0800

    Added assets compiler

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return array(
     'modules' => array(
         'Application',
         'Singly',
+        'SoliantAssetsCompiler',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
